4d3e5167b717300e33ffd6ad4080696f47e8d451
NGUYEN DUY HUNG <nguyenduyhung3624@gmail.com>
2025-09-16 18:12:46 +0700

    skeleton

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { supabase } from "@/services/supabase";
+import { Skeleton } from "antd";
 
 interface Product {
   id: number;
@@ -13,6 +14,7 @@ const Dashboard = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    document.title = "Dashboard - MyApp";
     const fetchLatestProducts = async () => {
       try {
         const { data, error } = await supabase
@@ -33,26 +35,31 @@ const Dashboard = () => {
     fetchLatestProducts();
   }, []);
 
-  if (loading) return <div>Đang tải...</div>;
+  if (loading)
+    return (
+      <div>
+        <Skeleton active paragraph={{ rows: 10 }} />
+      </div>
+    );
 
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-      <h2 className="text-xl mb-4">Sản phẩm mới nhất</h2>
+      <h2 className="text-xl mb-4">Latest Products</h2>
       {latestProducts.length > 0 ? (
         <ul className="space-y-4">
           {latestProducts.map((product) => (
             <li key={product.id} className="p-4 bg-gray-800 rounded-lg">
               <h3 className="font-semibold">{product.name}</h3>
-              <p>Giá: ${product.price}</p>
+              <p>Price{product.price}</p>
               <p>
-                Thêm vào: {new Date(product.created_at).toLocaleDateString()}
+                Added on {new Date(product.created_at).toLocaleDateString()}
               </p>
             </li>
           ))}
         </ul>
       ) : (
-        <p>Không có sản phẩm nào.</p>
+        <p>No products found</p>
       )}
     </div>
   );
